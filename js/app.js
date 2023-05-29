3b40084ded7aeb5ba2a58ab3a7f7dbbe7d84e7e8
shamilnezerov <shamil.nezerov@gmail.com>
2023-05-30 01:00:02 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function togglePopup() {
+  var popup = document.getElementById("WatchPopup");
+  if (popup.style.display === "none") {
+    popup.style.display = "inline";
+  } else {
+    popup.style.display = "none";
+  }
+}
+
+function language() {
+  var popup = document.getElementById("language");
+  if (popup.style.display === "none") {
+    popup.style.display = "inline";
+  } else {
+    popup.style.display = "none";
+  }
+}
+
+function siginin() {
+  var popup = document.getElementById("user_popup");
+  if (popup.style.display === "none") {
+    popup.style.display = "inline";
+  } else {
+    popup.style.display = "none";
+  }
+}
+function mobilemenu() {
+  var popup = document.getElementById("mobile-menu");
+  if (popup.style.display === "none") {
+    popup.style.display = "inline";
+  } else {
+    popup.style.display = "none";
+  }
+}
+
+function mobileLangPopup() {
+  var popup = document.getElementById("mobile_lang");
+  if (popup.style.display === "none") {
+    popup.style.display = "inline";
+  } else {
+    popup.style.display = "none";
+  }
+}
+
+
+
